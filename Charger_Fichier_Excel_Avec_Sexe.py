97d5b41e5c3b0eede9e0c2cbfed72bbf9f8aedb3
jouteur <laurent.francois.simplon@gmail.com>
2020-04-05 17:51:16 +0200

    Données avec genre et fichier de création du resultat

---
+++ Charger_Fichier_Excel_Avec_Sexe.py
@@ -0,0 +1,79 @@
+import database
+
+
+def mise_a_jour_genre(fichier):
+    # Initialisation du DataFrame df
+    # à partir des données du fichier CSV
+#    file = open('/home/laurent/git/turbine_project/Donnees/fichiers avec genre/1.csv', "r")
+    file = open(fichier, "r")
+    # utiliser readlines pour lire toutes les lignes du fichier
+    # La variable "lignes" est une liste contenant toutes les lignes du fichier
+    lines = file.readlines()
+    # fermez le fichier après avoir lu les lignes
+    file.close()
+    # Itérer sur les lignes
+
+    conn = database.create_connection()
+
+    for line in lines:
+        # if line.find("genre")<0:
+        pos_voie_id = line.find("VOIE_ID")
+        pos_2points = line.find(":", pos_voie_id)
+        pos_virgule = line.find(",", pos_2points)
+
+        pos_genre = line.find("genre\":")
+        pos_accolade = line.find("}}", pos_genre + 8)
+
+        print(line)
+        print(pos_voie_id, pos_2points, pos_virgule, pos_genre, pos_accolade)
+        index = line[pos_2points + 1:pos_virgule]
+        genre_str = line[pos_genre + 8: pos_accolade - 1]
+        voie_id_str = line[pos_voie_id + 9: pos_virgule]
+        print("voie_id_str: ", voie_id_str)
+        print(genre_str)
+
+        requete = "Update nom_des_voies set genre = '" + genre_str + "' where voie_id = " + voie_id_str + ";"
+        print(requete)
+        try:
+            database.query_create_select(conn, requete)
+        except:
+            print("Erreur")
+
+
+def sauve_base_de_donnes():
+    conn = database.create_connection()
+    requete = "Select * From nom_des_voies;"
+    requete = "Select voie_id, voie_complet, voie_fantoir, voie_date_cre, voie_real, voie_officiel, tenant, aboutissant, denom_annee, dm_seance, delib_num, cote_archives, denom_origine, lien_externe, observation, geojson, genre From nom_des_voies order by voie_id;"
+
+    try:
+        cur = database.query_create_select(conn, requete)
+        print("apres query")
+
+# voie_id, voie_complet, voie_fantoir, voie_date_cre, voie_real, voie_officiel, tenant, aboutissant, denom_annee, dm_seance, delib_num, cote_archives, denom_origine, lien_externe, observation, geojson, genre
+        fichier = open("sauvegarde_base_de_donnees.txt", "w")
+
+        for row in cur:
+            print(row[0])
+            ligne_fichier = ""
+            print("Début: ")
+            for j in range(0, 17):
+                print(row[j])
+                ligne_fichier = ligne_fichier + ";" + str(row[j])
+
+            print(ligne_fichier)
+            fichier.write(ligne_fichier[1:] + "\n")
+
+        fichier.close()
+
+
+    except Exception as inst:
+        print(type(inst))  # the exception instance
+        print(inst.args)  # arguments stored in .args
+
+
+#mise_a_jour_genre('/home/laurent/git/turbine_project/Donnees/fichiers avec genre/1.csv')
+#mise_a_jour_genre('/home/laurent/git/turbine_project/Donnees/fichiers avec genre/2.csv')
+#mise_a_jour_genre('/home/laurent/git/turbine_project/Donnees/fichiers avec genre/3.csv')
+#mise_a_jour_genre('/home/laurent/git/turbine_project/Donnees/fichiers avec genre/4.csv')
+
+sauve_base_de_donnes()
--- street.py
+++ street.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 import folium
+import database
 
 # Dessine une line polygonale à partir des coordonnées GPS contenues
 # dans l'objet GeoJSON
@@ -21,32 +22,55 @@ def draw_polyline(geo_json, map, color="blue", weight=5, opacity=0.6):
         folium.PolyLine(points, color=color, weight=weight, opacity=opacity).add_to(map)
 
 # Création d'une carte centrée sur Grenoble
-fmap = folium.Map(location=[45.1875602, 5.7357819], tiles="OpenStreetMap", zoom_start=13)
+fmap = folium.Map(location=[45.1875602, 5.7357819], tiles="OpenStreetMap", zoom_start=20)
 
 # Initialisation du DataFrame df
 # à partir des données du fichier CSV
-df = pd.read_csv('PLANDEVILLE_VOIES_VDG.csv', sep = ',')
+#df = pd.read_csv('PLANDEVILLE_AXES-DE-VOIES_VDG_EPSG4326.json', sep = ',')
 
 # Dessin de la rue N°67 : Avenue Jean Perrot
 
 try:
     # La j'affiche les rues en bleu ou en rouge si l'index est pair ou impair (pour voir)
     # je vais modifier ce code pour aller chercher en base de données le sexe (male = bleu, female = rose
-    for i in range(1, 927):
-        if (i % 2) == 0:
-            draw_polyline(df['GeoJSON'][i], fmap, "blue")
+    conn = database.create_connection()
+
+    cur = database.query_create_select(conn, "select genre, geojson From nom_des_voies where geojson not like '%[[[%';")
+
+    print("Connected to database")
+    list_coord = []
+
+    for ligne in cur:
+        geojson = ligne[1]
+        print(ligne)
+        sexe = ligne[0]
+
+        if sexe == "masculin":
+            couleur = "blue"
+        elif sexe == "feminin":
+            couleur = "red"
         else:
-            draw_polyline(df['GeoJSON'][i], fmap, "purple")
+            couleur = "gray"
 
-    # draw_polyline(df['GeoJSON'][112], fmap, "red")
+        draw_polyline(geojson, fmap, couleur)
 
-    # Compteur avec la valeur de la ligne excel - 2
+
+
+
+
+#    for i in range(1, 927):
+        #        print(i)
+        #if (i % 2) == 0:
+        #    draw_polyline(df['GeoJSON'][i], fmap, "blue")
+        #else:
+        #    if i == 249: print(df['GeoJSON'][i])
+#    draw_polyline(df['GeoJSON'][i], fmap, "purple")
+
+
+     # draw_polyline(df['GeoJSON'][112], fmap, "red")
+
 except Exception as e:
     print(e)
-
-
-
-
 
 
 # Sauvegarde de la carte dans un fichier HTML
